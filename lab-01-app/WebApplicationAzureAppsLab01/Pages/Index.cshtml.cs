e5ec97bfd29417bf32afd4f260e2fe29fc6f47c2
Jakub Sýkora <sykorajak@icloud.com>
2022-04-02 20:07:38 +0200

    Labs 01, 03, 04, 05

---
+++ lab-01-app/WebApplicationAzureAppsLab01/Pages/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApplication.Pages
+{
+	public class IndexModel : PageModel
+	{
+		private readonly string AzureStorageConnectionString;
+		private readonly string AzureStorageContainerName;
+
+		public List<BlobItem>? Blobs { get; set; } = new();
+
+		public IndexModel(IConfiguration configuration)
+		{
+			AzureStorageConnectionString = configuration.GetConnectionString("AzureStorageConnectionString");
+			AzureStorageContainerName = configuration.GetValue<string>("AzureStorageContainerName");
+		}
+
+		public void OnGet()
+		{
+			Blobs = CreateClient().GetBlobs().ToList();
+		}
+
+		public FileStreamResult OnGetDownloadBlob(string blobName)
+		{
+			var blobClient = CreateClient().GetBlobClient(blobName);
+			var contentType = blobClient.GetProperties().Value.ContentType;
+			var stream = blobClient.OpenRead();
+			return new FileStreamResult(stream, contentType);
+		}
+
+		private BlobContainerClient CreateClient()
+		{
+			return new Azure.Storage.Blobs.BlobContainerClient(AzureStorageConnectionString, AzureStorageContainerName);
+		}
+	}
+}
